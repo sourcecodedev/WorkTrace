02f2a06c59eb8779448361651f0fd30d68626654
romario.paucar.contractor <romario.paucar.contractor@bbva.com>
2025-05-21 20:19:07 -0500

    add activity

---
+++ app/src/main/java/com/upc/worktrace/RastrearAsistenciaActivity.kt
@@ -0,0 +1,12 @@
+package com.upc.worktrace
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RastrearAsistenciaActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rastreo_asistencia)
+
+    }
+}
